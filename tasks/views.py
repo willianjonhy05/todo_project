97d605357e65a956e9f621b7aa80a9b230bc6257
willianjohny05 <willianjohny05@gmail.com>
2024-11-04 20:49:22 -0300

    UC-14

---
+++ tasks/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        # Filtra as tarefas do usuário autenticado
+        return self.queryset.filter(user=self.request.user)
+    
